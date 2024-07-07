dc50c89fcb23cabfef0ad12bb8eef577c63a663b
AVLTG <aa.tolooei@gmail.com>
2024-07-07 11:27:17 -0400

    Commiting First two files, as well as pictures used for testing and results

---
+++ CV2EdgeDetection.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Load image
+img = cv2.imread('pexels-annpoan-5797899.jpg')
+# Convert to grayscale (even though image is already black and white)
+edges = cv2.Canny(img, 100, 200, L2gradient=True)
+# Display image
+plt.figure()
+plt.title('Notebook opened on desk near books')
+# Save image
+plt.imsave('CannyEdgeNotebookOpenedOnDeskNearBooks.png', edges, cmap='gray', format='png')
+plt.imshow(edges, cmap='gray')
+plt.show()
---
+++ CVContourDetection.py
@@ -0,0 +1,27 @@
+import cv2
+import matplotlib.pyplot as plt
+image = cv2.imread('K12_ 6Triangles_1_EG.png')
+cv2.waitKey(0)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+edged = cv2.Canny(gray, 30, 200)
+cv2.waitKey(0)
+
+contours, hierarchy = cv2.findContours(edged,
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow('Canny Edges After Contouring', edged)
+cv2.waitKey(0)
+
+print("Number of Contours = " + str(len(contours)))
+
+x = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+plt.figure()
+plt.title('Notebook opened on desk near books')
+plt.imsave('TriangleEdgeContours.png', x, cmap='gray', format='png')
+
+cv2.imshow('Contours', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
